3eaff53bb562cb2bb4466253039c60c824a3eebb
Andrés Gordo <andresgordoortiz@gmail.com>
2024-01-27 16:32:51 +0100

    Fix kallisto version checker Release 3.0 Add Trim_galore compatibility

--- AutomatedKallistoGeneAligner.sh
+++ AutomatedKallistoGeneAligner.sh
@@ -2,7 +2,7 @@
 
 echo "----Welcome to Automated Kallisto Gene alignment [AKG]----"
 echo "-- Made by Andrés Gordo, 2023-2024 --"
-echo "This script will align all your samples in a new folder using Kallisto, and checking their quality with FastQC and MultiQC."
+echo "This script will align all your samples in a new folder using Kallisto, and checking their quality with FastQC and MultiQC. Optionally, it can also use trim_galore for quality trimming before FastQC."
 
 # Check if mamba is installed
 if ! command -v mamba &> /dev/null; then
@@ -14,7 +14,7 @@ fi
 # Check if kallisto is installed
 if mamba list | grep -q "kallisto"; then
     installed_version=$(mamba list | awk '/kallisto/ {print $2}')
-    if [ "$installed_version" != "0.48" ]; then
+    if [ "$installed_version" != "0.48.0" ]; then
         echo "kallisto is installed, but not version 0.48. Installing kallisto=0.48..."
         mamba install -c bioconda "kallisto=0.48"
     else
@@ -37,19 +37,32 @@ if ! mamba list | grep -q "multiqc"; then
     mamba install -c bioconda "multiqc"
 fi
 
+# Check if trim_galore is installed
+if ! mamba list | grep -q "trim-galore"; then
+    echo "trim_galore is not installed. Installing trim_galore..."
+    mamba install -c bioconda "trim_galore"
+fi
+
 echo "All required packages are installed."
 
-# Create the folders for the output
-mkdir -p ~/new_AKG
-mkdir -p ~/new_AKG/fastqc
-mkdir -p ~/new_AKG/kallisto
+
 
 
 # Prompt the user for the input folder
+read -p "Name you new RNAseq experiment: " experiment_name
 read -p "Enter the absolute path (use realpath) to the input folder (containing *.gz files): " input_folder
+# Prompt user for FASTQ file extension
+echo "Enter the extension of the FASTQ files (e.g., .fq.gz or .fastq.gz):"
+read -r file_extension
 
+read -p "Do you want to use trim_galore for quality trimming before FastQC? (y/n): " use_trim
 # Prompt the user to choose between single-end and paired-end data
 read -p "Enter 's' for single-end data or 'p' for paired-end data: " data_type
+
+# Create the folders for the output
+mkdir -p ~/"$experiment_name"
+mkdir -p ~/"$experiment_name"/fastqc
+mkdir -p ~/"$experiment_name"/kallisto
 
 if [ "$data_type" = "p" ]; then
     read -p "Enter the extension for the first paired-end sample (e.g., _f1): " ext1
@@ -69,20 +82,62 @@ else
     # Prompt the user for the output folder
     read -p "Enter the absolute path to the reference genome file (containing a *.fa file): " genome_file
     
-    mkdir -p ~/new_AKG/index
+    mkdir -p ~/"$experiment_name"/index
     echo "--> AKG will now create an index based on your reference genome"
 
     # Create the index for the reference genome
-    cd ~/new_AKG/index || exit
+    cd ~/"$experiment_name"/index || exit
     kallisto index -i Homo_sapiens.GRCh38.cdna.all.index "$genome_file"
     echo "--> AKG has finished creating the index"
 
     # Get the index realpath
-    index_file=$(realpath "$(find ~/new_AKG/index -type f -name "*.index" -print -quit)")
+    index_file=$(realpath "$(find ~/"$experiment_name"/index -type f -name "*.index" -print -quit)")
 fi
 
 echo "Using index file: $index_file"
 echo "Input folder:  $input_folder"
+
+
+
+# Trim_galore and move files to trim_galore_output folder
+if [ "$use_trim" = "y" ]; then
+    mkdir -p ~/"$experiment_name"/trim_galore_output
+    echo "--> AKG will now use trim_galore for quality trimming before FastQC"
+    cd "$input_folder" || exit
+
+    if [ "$data_type" = "s" ]; then
+        # Single-end data
+        for input_file in "$input_folder"/*"$file_extension"; do
+            base_name=$(basename "$input_file" "$file_extension")
+            echo "$base_name"
+            trim_galore -o ~/"$experiment_name"/trim_galore_output -j 4 "$input_file"
+            mv ~/"$experiment_name"/trim_galore_output/"$base_name"_trimmed.fq.gz ~/"$experiment_name"/trim_galore_output/"$base_name""$file_extension"
+        done
+    elif [ "$data_type" = "p" ]; then
+        # Paired-end data
+        for input_file1 in "$input_folder"/*"$ext1""$file_extension"; do
+            base_name=$(basename -s "$ext1$file_extension" "$input_file1")
+            echo "$base_name"
+            input_file2="$input_folder/$base_name$ext2$file_extension"
+            
+            if [ -e "$input_file2" ]; then
+                trim_galore --paired -o ~/"$experiment_name"/trim_galore_output -j 4 "$input_file1" "$input_file2"
+                mv ~/"$experiment_name"/trim_galore_output/"$base_name""$ext1"_val_1.fq.gz ~/"$experiment_name"/trim_galore_output/"$base_name""$ext1""$file_extension"
+                mv ~/"$experiment_name"/trim_galore_output/"$base_name""$ext2"_val_2.fq.gz ~/"$experiment_name"/trim_galore_output/"$base_name""$ext2""$file_extension"
+
+            else
+                echo "Warning: Paired-end file for $base_name not found. Skipping..."
+            fi
+        done
+    else
+        echo "Invalid data type. Please choose 's' for single-end or 'p' for paired-end data."
+        exit 1
+    fi
+
+    input_folder=~/"$experiment_name"/trim_galore_output
+    echo "--> AKG has finished trimming with trim_galore"
+fi
+
 
 # Analyze .gz files with FastQC
 echo "--> AKG will now analyze the quality of your samples with FastQC"
@@ -91,16 +146,16 @@ fastqc *.gz -t $threads
 
 # Move files to the fastqc folder
 cd $input_folder || exit
-mv *fastqc* ~/new_AKG/fastqc
+mv *fastqc* ~/"$experiment_name"/fastqc
 echo "--> AKG has finished analyzing the quality of your samples with FastQC"
 
-cd ~/new_AKG/kallisto || exit
+cd ~/"$experiment_name"/kallisto || exit
 
 # Iterate through all ".gz" files in the input folder
 if [ "$data_type" = "s" ]; then
     # Single-end data
     for input_file in "$input_folder"/*.gz; do
-        base_name=$(basename -s .fastq.gz "$input_file")
+        base_name=$(basename -s "$file_extension" "$input_file")
         sample_output_folder="$base_name"
         mkdir -p "$sample_output_folder"
         echo "-> The sample $base_name is being aligned now by Kallisto"
@@ -110,9 +165,9 @@ if [ "$data_type" = "s" ]; then
 elif [ "$data_type" = "p" ]; then
     # Paired-end data
 
-    for input_file1 in "$input_folder"/*"$ext1".fq.gz; do
-        base_name=$(basename -s "$ext1".fq.gz "$input_file1")
-        input_file2="$input_folder/$base_name$ext2.fq.gz"
+    for input_file1 in "$input_folder"/*"$ext1""$file_extension"; do
+        base_name=$(basename -s "$ext1""$file_extension" "$input_file1")
+        input_file2="$input_folder/$base_name$ext2""$file_extension"
         
         if [ -e "$input_file2" ]; then
             sample_output_folder="$base_name"
@@ -132,6 +187,6 @@ fi
 echo "--> AKG has now Finished processing all your samples"
 echo "Summarising results via MultiQC"
 
-cd ~/new_AKG || exit
+cd ~/"$experiment_name" || exit
 multiqc -d .
 echo "AKG has finished, the final report has been produced alongside with the pseudoalignments"
