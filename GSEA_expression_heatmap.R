2ebb417c2c793b1a5ef9e16541af7a66afbd8cf0
Andrés Gordo <andresgordoortiz@gmail.com>
2024-01-28 12:16:35 +0100

    Initial version

---
+++ GSEA_expression_heatmap.R
@@ -0,0 +1,15 @@
+## Subset by Pathways
+
+myGSEA.df_H[myGSEA.df_H$ID=="HALLMARK_G2M_CHECKPOINT", 11]
+### extract values from that list, deleting the "/" symbol
+geneset_list<-unlist(myGSEA.df_H[myGSEA.df_H$ID=="HALLMARK_G2M_CHECKPOINT", 11]) %>% strsplit("/")
+geneset_list<-geneset_list$core_enrichment
+g = intersect(geneset_list, rownames(diffGenes))
+g_index<-rownames(diffGenes) %in% g
+geneset_expr<-diffGenes[g,]
+s = rowttests(diffGenes, factor(condition))[, "statistic"]
+## Complex heatmap for pathways
+Heatmap(geneset_expr, name = "z-score", top_annotation = HeatmapAnnotation(Condition = condition),
+        column_title = "HALLMARK_G2M_CHECKPOINT",
+        right_annotation = rowAnnotation("T Test" = anno_barplot(s[g_index], axis_param = list(direction = "reverse"), width = unit(2, "cm"))),
+        column_split = condition)
